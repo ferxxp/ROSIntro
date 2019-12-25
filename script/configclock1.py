aa7f13a7b651b585549be3f67466c70a1f86a19c
FQuevedo <nandoquev@gmail.com>
2019-12-25 23:13:26 +0100

    Added roslogs with colors Added test launch files

--- script/configclock1.py
+++ script/configclock1.py
@@ -1,4 +1,9 @@
 import time
+from std_msgs.msg import String, Bool
+import rospy
 start=time.time()
 minutesgone=1
 a=False
+msg=Bool()
+msg.data=True
+pub = rospy.Publisher('still_alive', Bool, queue_size=10)
